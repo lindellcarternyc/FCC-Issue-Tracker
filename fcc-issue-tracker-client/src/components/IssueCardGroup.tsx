a163badee7e43f6dc2cc7f70b795b8d8fbcc5d80
Lindell Carter <lindellcarternyc@gmail.com>
2018-09-09 15:29:20 -0400

    add issue to props

--- fcc-issue-tracker-client/src/components/IssueCardGroup.tsx
+++ fcc-issue-tracker-client/src/components/IssueCardGroup.tsx
@@ -2,28 +2,33 @@ import * as React from 'react'
 
 import { StyleProperties } from '../styles'
 
+import { IssueModel } from '../models/IssueModel'
 import IssueCard from './IssueCard'
 
-const IssueCardGroup = () => {
-  const IssueCardGroupStyles: StyleProperties = {
-    width: 'calc(100% - 40px)',
-    maxWidth: '940px',
-    margin: '0 auto',
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'space-evenly',
-  }
 
+const IssueCardGroupStyles: StyleProperties = {
+  width: 'calc(100% - 40px)',
+  maxWidth: '940px',
+  margin: '0 auto',
+  display: 'flex',
+  flexWrap: 'wrap',
+  justifyContent: 'space-evenly',
+}
+
+interface IssueCardGroupProps {
+  issues: IssueModel[]
+
+  onClickEdit: (id: string) => void
+}
+
+const IssueCardGroup = (props: IssueCardGroupProps) => {
+  
+  const issueCards = props.issues.map(issue => {
+    return <IssueCard key={issue._id} issue={issue} onClickEdit={props.onClickEdit} />
+  })
   return (
     <section style={IssueCardGroupStyles}>
-      {(() => {
-        const cards: JSX.Element[] = []
-        for ( let i = 0; i < 10; i++) {
-          const card = <IssueCard key={i}/>
-          cards.push(card)
-        }
-        return cards
-      })()}
+      {issueCards}
     </section>
   )
 }
