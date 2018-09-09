b14116e32020bdadd261157758105a479e9849a2
Lindell Carter <lindellcarternyc@gmail.com>
2018-09-09 13:41:41 -0400

    create issue fields

---
+++ fcc-issue-tracker-client/src/constants/issue-fields.ts
@@ -0,0 +1,21 @@
+import { InputProps } from '../components/Input'
+
+const IssueFields: InputProps[] = [
+{
+  name: 'issueTitle', label: 'Issue Title'
+},
+{
+  name: 'issueText', label: 'Issue Text'
+},
+{
+  name: 'createdBy', label: 'Created By'
+},
+{
+  name: 'assignedTo', label: 'Assigned To'
+},
+{
+  name: 'statusText', label: 'Status Text'
+}
+]
+
+export default IssueFields
