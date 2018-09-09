1323f7714b1a4cd468e9d982a2571f308d15d12e
Lindell Carter <lindellcarternyc@gmail.com>
2018-09-09 12:57:53 -0400

    create styles module

---
+++ fcc-issue-tracker-client/src/styles.ts
@@ -0,0 +1,12 @@
+export type StyleProperties = React.CSSProperties
+export interface StyleGroup {
+  [name: string]: StyleProperties
+}
+
+// Constants
+// Colors
+const DarkBlue = '#273043'
+
+export const Colors = {
+  DarkBlue
+}
